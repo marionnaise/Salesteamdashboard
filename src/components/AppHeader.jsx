0da1d625cfbe1b0fa7e12a5b0cf6a36fbc51b15a
marionnaise <marionrochaspro@gmail.com>
2026-03-02 15:52:43 +0100

    Creating app header component

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+export default function AppHeader() {
+    return(
+        <>
+        <h1 className="landing-header landing-title">Devis +</h1>
+        <h2 className="landing-header landing-slogan">Saisissez un montant, validez, c'est tracké
+        </h2>
+        </>
+    )
+};
